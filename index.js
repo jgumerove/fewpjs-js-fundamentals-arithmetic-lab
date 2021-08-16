3182e0ec4af4de2de30926d889a82f2ccaa5b450
jgumerove <josh.gumerove@gmail.com>
2021-08-16 17:32:45 -0400

    commit tree db09b89f22b2fa979cdd4f0fe61874756a9146f3

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 100)
+
+const num3 = 24
+const num4 = 20
+
+const mod = num3 % num4
+
+const max = Math.max(1, 4, 20, 19, 18)
